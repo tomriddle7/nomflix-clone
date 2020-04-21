591ccdf856f1d9b834228f1a11d2949e81d7718e
tomriddle7 <tomriddle7@naver.com>
2020-04-22 08:10:10 +0900

    Update react-helmet

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 import Loader from "Components/Loader";
 
 const Container = styled.div`
