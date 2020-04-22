6e92771515eda77e3079570dab63310c351c3069
tomriddle7 <tomriddle7@naver.com>
2020-04-22 10:11:57 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
-    api_key: "10923b261ba94d897ac6b81148314a3f",
+    api_key: "011b41fa7c237724c32e4dc0ab2749ae",
     language: "en-US"
   }
 });
